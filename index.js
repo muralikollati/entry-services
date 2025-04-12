c4e5db727ae346384b456f2d794e47ceb46240c4
Murali <murali@Muralis-MacBook-Pro.local>
2025-04-12 19:46:24 +0530

    deployement fix test

---
+++ index.js
@@ -0,0 +1,16 @@
+// api/index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel at entry-service");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// Export as a serverless function
+module.exports = (req, res) => {
+  app(req, res);
+};
