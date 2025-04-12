8b2e1015809f6b9e5d606bae2105e8acf7879027
Murali <murali@Muralis-MacBook-Pro.local>
2025-04-12 16:26:07 +0530

    Initial commit

---
+++ auth/verifyAuth.js
@@ -0,0 +1,20 @@
+const { admin } = require("../service/firebase");
+
+const verifyAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Missing or invalid token" });
+  }
+
+  const idToken = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Unauthorized: Invalid token", details: err.message });
+  }
+};
+
+module.exports = verifyAuth;
